bf3c48a8fdc3b1ef0ac02946206a245a3854ce50
cullenjohn <cullenjohn167@gmail.com>
2023-08-12 11:56:22 +0100

    fjifh

--- Test.py
+++ Test.py
@@ -1,7 +1,5 @@
-import json
 import os
 import pyodbc
-# import azure.functions as func
 
 def get_connection_string():
     azure_db = os.environ.get('DB_CONNECTION_STRING')
@@ -9,17 +7,19 @@ def get_connection_string():
     
 def create_connection():
     return pyodbc.connect(get_connection_string())
- 
-connection = create_connection()
 
-cursor = connection.cursor()
+def main():
+    connection = create_connection()
+    cursor = connection.cursor()
 
-query = "SELECT * FROM FOODS"
-cursor.execute(query)
+    query = "SELECT * FROM FOODS"
+    cursor.execute(query)
 
-for row in cursor.fetchall():
-    print(row)
-    
-cursor.close()
-connection.close()
-    
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    connection.close()
+
+if __name__ == '__main__':
+    main()
